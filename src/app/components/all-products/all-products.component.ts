4323fbf90eabb52ddd59148a186408985bce4cfb
Junior Marquez <junior.marquez.hsc@gmail.com>
2018-10-22 16:36:02 -0500

    selector

--- src/app/components/all-products/all-products.component.ts
+++ src/app/components/all-products/all-products.component.ts
@@ -8,34 +8,30 @@ import { ProductsService } from "../../services/products.service";
 })
 
 export class AllProductsComponent {
-productsFil:any[]=[];
-menu:any={};
-
+	productsFil:any[]=[];
+	menu:any={};
 	constructor(public _ps:ProductsService) {
-  	this.ngSort,this.send}
-  	
+  		this.ngSort,this.send
+  	}
   	public send(producto){
   		alert(""+producto.productName);
-  	}
-
+  	}	
   	public ngSort(category){
-  	this.productsFil=[];
+	  	this.productsFil=[];
   		for (var i=0;i<this._ps.products.length;i++){
-
   			if (this._ps.products[i].category.includes(category))
   			{
   				this.productsFil.push(this._ps.products[i]);
   				console.log("agregado"+this._ps.products[i].productName);
   			}
   		}
-  	}
-
+	}
 	ngOnInit(): void {
 		this.productsFil=this._ps.products;	
 		console.log(this._ps.products);	
- 	$.getScript('assets/js/collage.js');
- 	$.getScript('assets/js/scripts.js');
-  }
+ 		$.getScript('assets/js/collage.js');
+ 		$.getScript('assets/js/scripts.js');
+  	}
 }	
  
 
